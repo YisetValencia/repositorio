98f5d96869792eee18566a3627fa37f970f16d3f
YisetValencia <yisetvalencia170@gmail.com>
2026-02-05 11:40:32 -0500

    feat: Creacion de main con funcion sumar

---
+++ main.py
@@ -0,0 +1,15 @@
+def sumar_dos_numeros(numero1: int, numero2: int) -> int:
+    """
+    Suma de dos números y devuelve el resultado
+
+    Args:
+        numero1 (int): Primer número a sumar
+        numero2 (int): Segundo número a sumar
+
+    Returns:
+        int: La suma de numero1 y numero2
+    """
+    return numero1 + numero2
+
+
+print(sumar_dos_numeros(2, 4))
